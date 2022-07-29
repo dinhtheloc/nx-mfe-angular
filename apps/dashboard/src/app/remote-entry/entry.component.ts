1c9b21eee0442c247e2116fc82a5c24e95a7e901
locdt2 <locdtptit@gmail.com>
2022-07-29 16:13:14 +0700

    init project

---
+++ apps/dashboard/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'locdt-dashboard-entry',
+  template: `<locdt-nx-welcome></locdt-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
